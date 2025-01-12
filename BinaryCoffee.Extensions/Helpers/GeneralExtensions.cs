b4626da42921db4b481b4aea2d6f0719689fd102
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2025-01-11 20:21:32 -0600

    Add GeneralExtensions.cs with general utility methods

---
+++ BinaryCoffee.Extensions/Helpers/GeneralExtensions.cs
@@ -0,0 +1,16 @@
+﻿#nullable enable
+
+using System.Collections.Generic;
+
+namespace BinaryCoffee.Extensions.Helpers;
+
+public static class AdditionalExtensions
+{
+    public static void AddNested<TList, TKey, TValueItem>(this Dictionary<TKey, TList> listHash, TKey key, TValueItem valueItem) where TList : ICollection<TValueItem>, new()
+    {
+        if (listHash.TryGetValue(key, out var valueItems))
+            valueItems.Add(valueItem);
+        else
+            listHash.Add(key, new() { valueItem });
+    }
+}
